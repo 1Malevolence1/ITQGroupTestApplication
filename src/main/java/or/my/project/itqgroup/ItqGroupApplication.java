7d52367a75695bd9d161a7b2f016a77a1e5b52cd
Daniil Vladimirov <danil26rusdanil@gmail.com>
2026-01-29 16:11:07 +0300

    init project

---
+++ src/main/java/or/my/project/itqgroup/ItqGroupApplication.java
@@ -0,0 +1,13 @@
+package or.my.project.itqgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItqGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItqGroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/or/my/project/itqgroup/ItqGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package or.my.project.itqgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItqGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
